18799103d2959154dfef163d42ef6793950e489c
vauxhall <zero.griffin@gmail.com>
2017-06-10 18:20:59 -0500

    layout view home

--- miniMancera/View/Home/VHome.swift
+++ miniMancera/View/Home/VHome.swift
@@ -2,5 +2,67 @@ import UIKit
 
 class VHome:View
 {
+    private weak var viewOptions:VHomeOptions!
+    private let kHeaderHeight:CGFloat = 200
+    private let kFooterHeight:CGFloat = 100
     
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        
+        guard
+        
+            let controller:CHome = self.controller as? CHome
+        
+        else
+        {
+            return
+        }
+        
+        let viewHeader:VHomeHeader = VHomeHeader(controller:controller)
+        
+        let viewOptions:VHomeOptions = VHomeOptions(controller:controller)
+        self.viewOptions = viewOptions
+        
+        let viewFooter:VHomeFooter = VHomeFooter(controller:controller)
+        
+        addSubview(viewHeader)
+        addSubview(viewOptions)
+        addSubview(viewFooter)
+        
+        NSLayoutConstraint.topToTop(
+            view:viewHeader,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewHeader,
+            constant:kHeaderHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewHeader,
+            toView:self)
+        
+        NSLayoutConstraint.topToBottom(
+            view:viewOptions,
+            toView:viewHeader)
+        NSLayoutConstraint.bottomToTop(
+            view:viewOptions,
+            toView:viewFooter)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewOptions,
+            toView:self)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:viewFooter,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:viewFooter,
+            constant:kFooterHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:viewFooter,
+            toView:self)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
