cc9686d040e8c7f9d26a8f6d409c34a2a8c3fe55
vauxhall <zero.griffin@gmail.com>
2017-06-10 18:25:44 -0500

    home options collection

--- miniMancera/View/Home/VHomeOptions.swift
+++ miniMancera/View/Home/VHomeOptions.swift
@@ -1,8 +1,9 @@
 import UIKit
 
-class VHomeOptions:UIView
+class VHomeOptions:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
     private weak var controller:CHome!
+    private weak var collectionView:VCollection!
     
     init(controller:CHome)
     {
@@ -11,6 +12,24 @@ class VHomeOptions:UIView
         backgroundColor = UIColor.clear
         translatesAutoresizingMaskIntoConstraints = false
         self.controller = controller
+        
+        let collectionView:VCollection = VCollection()
+        collectionView.alwaysBounceHorizontal = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCell(cell:VHomeOptionsCell.self)
+        self.collectionView = collectionView
+        
+        if let flow:VCollectionFlow = collectionView.collectionViewLayout as? VCollectionFlow
+        {
+            flow.scrollDirection = UICollectionViewScrollDirection.horizontal
+        }
+        
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.equals(
+            view:collectionView,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
