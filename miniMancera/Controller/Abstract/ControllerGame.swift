17bd9c790a44aba61f63546fcfc565a4018189ac
vauxhall <zero.griffin@gmail.com>
2017-06-10 19:37:10 -0500

    loading sk view

--- miniMancera/Controller/Abstract/ControllerGame.swift
+++ miniMancera/Controller/Abstract/ControllerGame.swift
@@ -3,5 +3,31 @@ import SpriteKit
 
 class ControllerGame:UIViewController
 {
+    override func loadView()
+    {
+        let view:SKView = SKView(frame:CGRect.zero)
+        self.view = view
+    }
     
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        let scene:SKScene = SKScene(size:view.bounds.size)
+        
+        guard
+            
+            let view:SKView = self.view as? SKView
+        
+        else
+        {
+            return
+        }
+        
+        scene.scaleMode = SKSceneScaleMode.aspectFill
+        view.showsFPS = true
+        view.showsNodeCount = true
+        view.ignoresSiblingOrder = true
+        view.presentScene(scene)
+    }
 }
