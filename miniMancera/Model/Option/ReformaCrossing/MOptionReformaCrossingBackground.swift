7ece70b8f97fd0a8788be4c4e39dfc716fbf2655
vauxhall <zero.griffin@gmail.com>
2017-06-11 05:45:47 -0500

    fixed background size

--- miniMancera/Model/Option/ReformaCrossing/MOptionReformaCrossingBackground.swift
+++ miniMancera/Model/Option/ReformaCrossing/MOptionReformaCrossingBackground.swift
@@ -3,22 +3,22 @@ import SpriteKit
 
 class MOptionReformaCrossingBackground:SKSpriteNode
 {
+    private let kZPosition:CGFloat = -1
+    
     init(size:CGSize)
     {
         let texture:SKTexture = SKTexture(image:#imageLiteral(resourceName: "assetReformaCrossingBackground"))
         let textureSize:CGSize = texture.size()
         
-        super.init(texture:texture, color:UIColor.clear, size:size)
-        zPosition = -1
+        super.init(texture:texture, color:UIColor.clear, size:textureSize)
         
-        let width:CGFloat = textureSize.width
-        let height:CGFloat = textureSize.height
         let screenWidth:CGFloat = size.width
         let screenHeight:CGFloat = size.height
         let screenWidth_2:CGFloat = screenWidth / 2.0
         let screenHeight_2:CGFloat = screenHeight / 2.0
         let centerPosition:CGPoint = CGPoint(x:screenWidth_2, y:screenHeight_2)
         position = centerPosition
+        zPosition = kZPosition
     }
     
     required init?(coder:NSCoder)
