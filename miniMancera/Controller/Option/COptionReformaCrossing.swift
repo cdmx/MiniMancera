a5e99dcc2c7944279a840bcb647cdd65beacb10a
vauxhall <zero.griffin@gmail.com>
2017-06-10 19:50:34 -0500

    reforma crossing model

--- miniMancera/Controller/Option/COptionReformaCrossing.swift
+++ miniMancera/Controller/Option/COptionReformaCrossing.swift
@@ -2,5 +2,15 @@ import UIKit
 
 class COptionReformaCrossing:ControllerGame
 {
+    init()
+    {
+        let model:MOptionReformaCrossing = MOptionReformaCrossing()
+        
+        super.init(model:model)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
