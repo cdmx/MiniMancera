b53b96f824fcd13c187871e421825d88097be64b
vauxhall <zero.griffin@gmail.com>
2017-06-11 14:33:08 -0500

    factory lanes

--- miniMancera/Model/Option/ReformaCrossing/MOptionReformaCrossing.swift
+++ miniMancera/Model/Option/ReformaCrossing/MOptionReformaCrossing.swift
@@ -3,6 +3,37 @@ import SpriteKit
 
 class MOptionReformaCrossing:MOptionProtocol
 {
+    let lanes:[MOptionReformaCrossingLaneProtocol]
+    
+    private class func factoryLanes() -> [MOptionReformaCrossingLaneProtocol]
+    {
+        let laneA:MOptionReformaCrossingLaneA = MOptionReformaCrossingLaneA()
+        let laneB:MOptionReformaCrossingLaneB = MOptionReformaCrossingLaneB()
+        let laneC:MOptionReformaCrossingLaneC = MOptionReformaCrossingLaneC()
+        let laneD:MOptionReformaCrossingLaneD = MOptionReformaCrossingLaneD()
+        let laneE:MOptionReformaCrossingLaneE = MOptionReformaCrossingLaneE()
+        let laneF:MOptionReformaCrossingLaneF = MOptionReformaCrossingLaneF()
+        let laneG:MOptionReformaCrossingLaneG = MOptionReformaCrossingLaneG()
+        let laneH:MOptionReformaCrossingLaneH = MOptionReformaCrossingLaneH()
+        
+        let lanes:[MOptionReformaCrossingLaneProtocol] = [
+            laneA,
+            laneB,
+            laneC,
+            laneD,
+            laneE,
+            laneF,
+            laneG,
+            laneH]
+        
+        return lanes
+    }
+    
+    init()
+    {
+        lanes = MOptionReformaCrossing.factoryLanes()
+    }
+    
     //MARK: option protocol
     
     func sceneWithSize(size:CGSize) -> SKScene
