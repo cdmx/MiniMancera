3009f036dc0f0f9a3572a5513130a94a356c7eab
vauxhall <zero.griffin@gmail.com>
2017-06-11 15:06:32 -0500

    showing title reforma

--- miniMancera/Model/Option/ReformaCrossing/MOptionReformaCrossingSceneTitle.swift
+++ miniMancera/Model/Option/ReformaCrossing/MOptionReformaCrossingSceneTitle.swift
@@ -3,6 +3,8 @@ import SpriteKit
 class MOptionReformaCrossingSceneTitle:SKScene
 {
     private weak var model:MOptionReformaCrossing!
+    private let kFont:String = "Arial-BoldMT"
+    private let kFontSize:CGFloat = 45
     
     init(model:MOptionReformaCrossing)
     {
@@ -11,10 +13,25 @@ class MOptionReformaCrossingSceneTitle:SKScene
         super.init(size:model.size)
         backgroundColor = SKColor.black
         
+        let width_2:CGFloat = size.width / 2.0
+        let height_2:CGFloat = size.height / 2.0
+        
         let background:MOptionReformaCrossingBackground = MOptionReformaCrossingBackground(
             size:size)
         
+        let levelNumber:NSNumber = model.level as NSNumber
+        let messageTitle:String = String(
+            format:NSLocalizedString("MOptionReformaCrossingSceneTitle_labelTitle", comment:""),
+            levelNumber)
+        
+        let labelTitle:SKLabelNode = SKLabelNode(fontNamed:kFont)
+        labelTitle.text = messageTitle
+        labelTitle.fontSize = kFontSize
+        labelTitle.fontColor = SKColor.white
+        labelTitle.position = CGPoint(x:width_2, y:height_2)
+        
         addChild(background)
+        addChild(labelTitle)
     }
     
     required init?(coder:NSCoder)
