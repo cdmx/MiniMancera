6486f40e0d02dbba7c66e48e704c47d0dc2e518b
vauxhall <zero.griffin@gmail.com>
2017-06-12 15:47:35 -0500

    controller show menu

--- miniMancera/Controller/Option/COptionReformaCrossing.swift
+++ miniMancera/Controller/Option/COptionReformaCrossing.swift
@@ -2,4 +2,40 @@ import UIKit
 
 class COptionReformaCrossing:ControllerGame<MOptionReformaCrossing>
 {
+    //MARK: public
+    
+    func showMenu()
+    {
+        let alert:UIAlertController = UIAlertController(
+            title:nil,
+            message:nil,
+            preferredStyle:UIAlertControllerStyle.actionSheet)
+        
+        let actionResume:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("COptionReformaCrossing_menuResume", comment:""),
+            style:
+            UIAlertActionStyle.default)
+        
+        let actionExit:UIAlertAction = UIAlertAction(
+            title:
+            NSLocalizedString("COptionReformaCrossing_menuExit", comment:""),
+            style:
+            UIAlertActionStyle.destructive)
+        { [weak self] (action:UIAlertAction) in
+            
+        }
+        
+        alert.addAction(actionResume)
+        alert.addAction(actionExit)
+        
+        if let popover:UIPopoverPresentationController = alert.popoverPresentationController
+        {
+            popover.sourceView = view
+            popover.sourceRect = CGRect.zero
+            popover.permittedArrowDirections = UIPopoverArrowDirection.up
+        }
+        
+        present(alert, animated:true, completion:nil)
+    }
 }
