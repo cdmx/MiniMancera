838fe6da1195ca3d64e30515252afc50efde5c6c
vauxhall <zero.griffin@gmail.com>
2017-06-14 01:14:14 -0500

    factory foes

--- miniMancera/Model/Option/ReformaCrossing/Foe/MOptionReformaCrossingFoeFactory.swift
+++ miniMancera/Model/Option/ReformaCrossing/Foe/MOptionReformaCrossingFoeFactory.swift
@@ -6,10 +6,26 @@ extension MOptionReformaCrossingFoe
         lane:MOptionReformaCrossingLane,
         controller:COptionReformaCrossing) -> MOptionReformaCrossingFoe
     {
+        let possibleFoes:[MOptionReformaCrossingFoe.Id] = lane.posibleFoes
+        
+        
+        
         let foe:MOptionReformaCrossingFoeVW = MOptionReformaCrossingFoeVW(
             lane:lane,
             controller:controller)
         
         return foe
+    }
+    
+    //MARK: private
+    
+    private class func foeIdFrom(lane:MOptionReformaCrossingLane) -> MOptionReformaCrossingFoe.Id
+    {
+        let possibleFoes:[MOptionReformaCrossingFoe.Id] = lane.posibleFoes
+        let countIds:UInt32 = UInt32(possibleFoes.count)
+        let randomId:Int = Int(arc4random_uniform(countIds))
+        let foeId:MOptionReformaCrossingFoe.Id = possibleFoes[randomId]
+        
+        return foeId
     }
 }
