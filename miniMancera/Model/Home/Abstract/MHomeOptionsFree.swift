b23bd400c1e4a9df146dbae8286011f8b503f2b0
vauxhall <zero.griffin@gmail.com>
2017-06-15 13:44:03 -0500

    options model free

--- miniMancera/Model/Home/Abstract/MHomeOptionsFree.swift
+++ miniMancera/Model/Home/Abstract/MHomeOptionsFree.swift
@@ -2,5 +2,60 @@ import UIKit
 
 class MHomeOptionsFree:MHomeOptionsProtocol
 {
+    private(set) weak var dataOption:DOptionFree?
     
+    var available:Bool
+    {
+        get
+        {
+            if dataOption == nil
+            {
+                return false
+            }
+            
+            return true
+        }
+    }
+    
+    var title:String?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    var descr:String?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    var thumbnail:UIImage?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    var splashImage:UIImage?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    required init(dataOption:DOption)
+    {
+        self.dataOption = dataOption as? DOptionFree
+    }
+    
+    func selected() -> UIViewController?
+    {
+        return nil
+    }
 }
--- miniMancera/Model/Home/Abstract/MHomeOptionsProtocol.swift
+++ miniMancera/Model/Home/Abstract/MHomeOptionsProtocol.swift
@@ -3,8 +3,11 @@ import UIKit
 protocol MHomeOptionsProtocol
 {
     var available:Bool { get }
-    var title:String { get }
-    var thumbnail:UIImage { get }
+    var title:String? { get }
+    var descr:String? { get }
+    var thumbnail:UIImage? { get }
+    var splashImage:UIImage? { get }
+    
     init(dataOption:DOption)
-    func gameController() -> UIViewController
+    func selected() -> UIViewController?
 }
