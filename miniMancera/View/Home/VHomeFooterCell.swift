e8149f1c0cdb3e64fa3fe8e527fa8ff24070a822
vauxhall <zero.griffin@gmail.com>
2017-06-15 00:01:37 -0500

    home footer cell label title

--- miniMancera/View/Home/VHomeFooterCell.swift
+++ miniMancera/View/Home/VHomeFooterCell.swift
@@ -2,11 +2,37 @@ import UIKit
 
 class VHomeFooterCell:UICollectionViewCell
 {
+    private weak var imageView:UIImageView!
+    private weak var labelTitle:UILabel!
+    private let kTitleHeight:CGFloat = 40
+    private let kAlphaSelected:CGFloat = 0.2
+    private let kAlphaNotSelected:CGFloat = 1
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         backgroundColor = UIColor.clear
+        
+        let labelTitle:UILabel = UILabel()
+        labelTitle.isUserInteractionEnabled = false
+        labelTitle.translatesAutoresizingMaskIntoConstraints = false
+        labelTitle.backgroundColor = UIColor.clear
+        labelTitle.textColor = UIColor.white
+        labelTitle.textAlignment = NSTextAlignment.center
+        self.labelTitle = labelTitle
+        
+        addSubview(labelTitle)
+        
+        NSLayoutConstraint.bottomToBottom(
+            view:labelTitle,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:labelTitle,
+            constant:kTitleHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:labelTitle,
+            toView:self)
     }
     
     required init?(coder:NSCoder)
@@ -14,10 +40,40 @@ class VHomeFooterCell:UICollectionViewCell
         return nil
     }
     
+    override var isSelected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var isHighlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        if isSelected || isHighlighted
+        {
+            alpha = kAlphaSelected
+        }
+        else
+        {
+            alpha = kAlphaNotSelected
+        }
+    }
+    
     //MARK: public
     
     func config(model:MHomeFooterProtocol)
     {
-        
+        hover()
     }
 }
