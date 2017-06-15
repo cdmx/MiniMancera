5257f8027db7a4c9adf52059d9cd25ff00371127
vauxhall <zero.griffin@gmail.com>
2017-06-15 02:54:43 -0500

    game protocol

---
+++ miniMancera/Model/Main/Abstract/MGameFactory.swift
@@ -0,0 +1,9 @@
+//
+//  MGameFactory.swift
+//  miniMancera
+//
+//  Created by zero on 6/15/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ miniMancera/Model/Main/Abstract/MGamePurchaseProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  MGamePurchaseProtocol.swift
+//  miniMancera
+//
+//  Created by zero on 6/15/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
