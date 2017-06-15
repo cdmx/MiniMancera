e3283192288ffec0e4ccb90bf28df9d47e1dc8f1
vauxhall <zero.griffin@gmail.com>
2017-06-15 13:45:04 -0500

    model options purchase

--- miniMancera/Model/Home/Abstract/MHomeOptionsPurchase.swift
+++ miniMancera/Model/Home/Abstract/MHomeOptionsPurchase.swift
@@ -2,5 +2,64 @@ import UIKit
 
 class MHomeOptionsPurchase:MHomeOptionsProtocol
 {
+    private(set) weak var dataOption:DOptionPurchase?
     
+    var available:Bool
+    {
+        get
+        {
+            guard
+            
+                let purchased:Bool = dataOption?.purchased
+            
+            else
+            {
+                return false
+            }
+            
+            return purchased
+        }
+    }
+    
+    var title:String?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    var descr:String?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    var thumbnail:UIImage?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    var splashImage:UIImage?
+    {
+        get
+        {
+            return nil
+        }
+    }
+    
+    required init(dataOption:DOption)
+    {
+        self.dataOption = dataOption as? DOptionPurchase
+    }
+    
+    func selected() -> UIViewController?
+    {
+        return nil
+    }
 }
