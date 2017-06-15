5746a49eb5bd90e055e755ef274e3beb45227bb9
vauxhall <zero.griffin@gmail.com>
2017-06-15 03:05:55 -0500

    model home polluted garden

---
+++ miniMancera/Model/Home/Options/MHomeOptionsReformaCrossing.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class MHomeOptionsReformaCrossing:MHomeOptionsProtocol
+{
+    private let kAvailable:Bool = true
+    
+    var available:Bool
+    {
+        get
+        {
+            return kAvailable
+        }
+    }
+    
+    var title:String
+    {
+        get
+        {
+            return NSLocalizedString("MHomeOptionsReformaCrossing_title", comment:"")
+        }
+    }
+    
+    var thumbnail:UIImage
+    {
+        get
+        {
+            return UIImage()
+        }
+    }
+    
+    func gameController() -> UIViewController
+    {
+        let controller:COptionReformaCrossing = COptionReformaCrossing()
+        
+        return controller
+    }
+}
