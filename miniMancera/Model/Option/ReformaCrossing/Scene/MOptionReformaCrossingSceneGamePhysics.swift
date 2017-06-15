82654418d2c15cd4a406f17268caff6caa85753d
vauxhall <zero.griffin@gmail.com>
2017-06-15 17:30:21 -0500

    extension of generics can't have @objc

--- miniMancera/Model/Option/ReformaCrossing/Scene/MOptionReformaCrossingSceneGamePhysics.swift
+++ miniMancera/Model/Option/ReformaCrossing/Scene/MOptionReformaCrossingSceneGamePhysics.swift
@@ -2,5 +2,54 @@ import SpriteKit
 
 extension MOptionReformaCrossingSceneGame:SKPhysicsContactDelegate
 {
+    //MARK: private
     
+    private func contactFoes(foeA:MOptionReformaCrossingFoe, foeB:MOptionReformaCrossingFoe)
+    {
+        if foeA.created < foeB.created
+        {
+            foeB.hitTheBreaks()
+        }
+        else
+        {
+            foeA.hitTheBreaks()
+        }
+    }
+    
+    private func contactPlayerFoe(player:MOptionReformaCrossingPlayer, foe:MOptionReformaCrossingFoe)
+    {
+        playSound(soundName:kSoundHonk)
+        
+        if controller.model.gameActive
+        {
+            controller.hitAndRun()
+        }
+    }
+    
+    //MARK: contact delegate
+    
+    func didBegin(_ contact:SKPhysicsContact)
+    {
+        let bodyA:SKNode? = contact.bodyA.node
+        let bodyB:SKNode? = contact.bodyB.node
+        
+        if let foeA:MOptionReformaCrossingFoe = bodyA as? MOptionReformaCrossingFoe
+        {
+            if let player:MOptionReformaCrossingPlayer = bodyB as? MOptionReformaCrossingPlayer
+            {
+                contactPlayerFoe(player:player, foe:foeA)
+            }
+            else if let foeB:MOptionReformaCrossingFoe = bodyB as? MOptionReformaCrossingFoe
+            {
+                contactFoes(foeA:foeA, foeB:foeB)
+            }
+        }
+        else if let player:MOptionReformaCrossingPlayer = bodyA as? MOptionReformaCrossingPlayer
+        {
+            if let foeB:MOptionReformaCrossingFoe = bodyB as? MOptionReformaCrossingFoe
+            {
+                contactPlayerFoe(player:player, foe:foeB)
+            }
+        }
+    }
 }
