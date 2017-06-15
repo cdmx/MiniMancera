54818102f94cc58a9299f991340e3f720b9b7dfa
vauxhall <zero.griffin@gmail.com>
2017-06-15 15:19:53 -0500

    controller polluted garden

--- miniMancera/Model/Home/Options/MHomeOptionsPollutedGarden.swift
+++ miniMancera/Model/Home/Options/MHomeOptionsPollutedGarden.swift
@@ -2,5 +2,19 @@ import UIKit
 
 class MHomeOptionsPollutedGarden:MHomeOptionsPurchase
 {   
-    
+    override func selected() -> UIViewController?
+    {
+        guard
+            
+            let dataOption:DOption = self.dataOption
+            
+        else
+        {
+            return nil
+        }
+        
+        let controller:COptionReformaCrossing = COptionReformaCrossing(dataOption:dataOption)
+        
+        return controller
+    }
 }
