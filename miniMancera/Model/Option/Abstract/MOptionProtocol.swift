76d2669233a91c8de11857f65cb1643dea57e4b2
vauxhall <zero.griffin@gmail.com>
2017-06-15 16:47:29 -0500

    model option scene game

--- miniMancera/Model/Option/Abstract/MOptionProtocol.swift
+++ miniMancera/Model/Option/Abstract/MOptionProtocol.swift
@@ -6,6 +6,7 @@ protocol MOptionProtocol
     var size:CGSize { get }
     var score:Int { get }
     var gameActive:Bool { get }
+    var soundBackground:String? { get }
     
     init()
     func sceneWithSize(controller:UIViewController, size:CGSize) -> SKScene?
