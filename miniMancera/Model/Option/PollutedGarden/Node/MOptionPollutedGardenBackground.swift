3193ac3984998511eb2d966b7cde256d204a37ff
vauxhall <zero.griffin@gmail.com>
2017-06-16 12:04:56 -0500

    background with swing

--- miniMancera/Model/Option/PollutedGarden/Node/MOptionPollutedGardenBackground.swift
+++ miniMancera/Model/Option/PollutedGarden/Node/MOptionPollutedGardenBackground.swift
@@ -14,10 +14,16 @@ class MOptionPollutedGardenBackground:SKSpriteNode
         
         super.init(texture:texture, color:UIColor.clear, size:textureSize)
         
+        let textureHeight:CGFloat = textureSize.height
         let sceneSize:CGSize = controller.model.size
         let sceneWidth:CGFloat = sceneSize.width
+        let sceneHeight:CGFloat = sceneSize.height
+        let sceneHeight_2:CGFloat = sceneHeight / 2.0
         let sceneWidth_2:CGFloat = sceneWidth / 2.0
-        let centerPosition:CGPoint = CGPoint(x:sceneWidth_2, y:sceneHeight_2)
+        let deltaHeight:CGFloat = textureHeight - sceneHeight
+        let deltaHeight_2:CGFloat = deltaHeight / 2.0
+        let positionY:CGFloat = deltaHeight_2 + sceneHeight_2
+        let centerPosition:CGPoint = CGPoint(x:sceneWidth_2, y:positionY)
         position = centerPosition
         zPosition = kZPosition
     }
--- miniMancera/Model/Option/PollutedGarden/Scene/MOptionPollutedGardenSceneGame.swift
+++ miniMancera/Model/Option/PollutedGarden/Scene/MOptionPollutedGardenSceneGame.swift
@@ -2,4 +2,18 @@ import SpriteKit
 
 class MOptionPollutedGardenSceneGame:MOptionSceneGame<MOptionPollutedGarden, COptionPollutedGarden>
 {
+    override init(controller:COptionPollutedGarden)
+    {
+        super.init(controller:controller)
+        
+        let background:MOptionPollutedGardenBackground = MOptionPollutedGardenBackground(
+            controller:controller)
+        
+        addChild(background)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
