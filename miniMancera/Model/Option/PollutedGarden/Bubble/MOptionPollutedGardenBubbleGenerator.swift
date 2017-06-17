5cf38dae608ea4c1450147a92fb8ebffb8de0c25
vauxhall <zero.griffin@gmail.com>
2017-06-17 01:21:29 -0500

    extension factory colours

--- miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubbleGenerator.swift
+++ miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubbleGenerator.swift
@@ -10,6 +10,6 @@ class MOptionPollutedGardenBubbleGenerator
     
     init()
     {
-        
+        bubbleTypes = MOptionPollutedGardenBubbleGenerator.factoryBubbleTypes()
     }
 }
---
+++ miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubbleGeneratorFactoryColours.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+extension MOptionPollutedGardenBubbleGenerator
+{
+    class func factoryColours() -> [UIColor]
+    {
+        let red:UIColor = UIColor.red
+        let blue:UIColor = UIColor.blue
+        
+        let colours:[UIColor] = [
+            red,
+            blue]
+        
+        return colours
+    }
+}
