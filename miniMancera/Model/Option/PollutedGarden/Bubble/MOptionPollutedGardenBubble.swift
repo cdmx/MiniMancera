45817aed968bf084203db89f4b12e2dcc8fa3546
vauxhall <zero.griffin@gmail.com>
2017-06-17 02:15:43 -0500

    start physics

--- miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubble.swift
+++ miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubble.swift
@@ -4,6 +4,7 @@ import SpriteKit
 class MOptionPollutedGardenBubble:SKSpriteNode
 {
     private weak var controller:COptionPollutedGarden!
+    private weak var bubbleType:MOptionPollutedGardenBubbleType!
     
     init(
         controller:COptionPollutedGarden,
@@ -18,11 +19,32 @@ class MOptionPollutedGardenBubble:SKSpriteNode
         xScale = bubbleType.orientation.rawValue
         colorBlendFactor = 1
         self.position = position
+        self.bubbleType = bubbleType
         self.controller = controller
+        
+        startPhysics()
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: private
+    
+    private func startPhysics()
+    {
+        let physicsBody:SKPhysicsBody = SKPhysicsBody(
+            circleOfRadius:bubbleType.size.width)
+        physicsBody.isDynamic = false
+        physicsBody.friction = 0
+        physicsBody.angularVelocity = 0
+        physicsBody.allowsRotation = true
+        physicsBody.restitution = 1
+        
+        physicsBody.categoryBitMask = MOptionReformaCrossingPhysicsStruct.Player
+        physicsBody.contactTestBitMask = MOptionReformaCrossingPhysicsStruct.Foe
+        physicsBody.collisionBitMask = MOptionReformaCrossingPhysicsStruct.Foe
+        self.physicsBody = physicsBody
     }
 }
