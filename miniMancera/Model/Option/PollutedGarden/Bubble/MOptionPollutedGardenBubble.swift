1d4dd55ac518a5e0787366eede503fb393965a36
vauxhall <zero.griffin@gmail.com>
2017-06-17 01:44:25 -0500

    random bubble

--- miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubble.swift
+++ miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubble.swift
@@ -5,7 +5,11 @@ class MOptionPollutedGardenBubble:SKSpriteNode
 {
     private weak var controller:COptionPollutedGarden!
     
-    init(controller:COptionPollutedGarden)
+    init(
+        controller:COptionPollutedGarden,
+        bubbleType:MOptionPollutedGardenBubbleType,
+        colour:UIColor,
+        position:CGPoint)
     {
         super.init(texture: <#T##SKTexture?#>, color: <#T##UIColor#>, size: <#T##CGSize#>)
     }
--- miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubbleGenerator.swift
+++ miniMancera/Model/Option/PollutedGarden/Bubble/MOptionPollutedGardenBubbleGenerator.swift
@@ -3,13 +3,61 @@ import SpriteKit
 
 class MOptionPollutedGardenBubbleGenerator
 {
+    private weak var controller:COptionPollutedGarden!
     private let bubbleTypes:[MOptionPollutedGardenBubbleType]
     private let colours:[UIColor]
     private let countBubbles:UInt32
     private let countColours:UInt32
     
-    init()
+    init(controller:COptionPollutedGarden)
     {
         bubbleTypes = MOptionPollutedGardenBubbleGenerator.factoryBubbleTypes()
+        colours = MOptionPollutedGardenBubbleGenerator.factoryColours()
+        countBubbles = UInt32(bubbleTypes.count)
+        countColours = UInt32(colours.count)
+        self.controller = controller
+    }
+    
+    //MARK: private
+    
+    private func randomType() -> MOptionPollutedGardenBubbleType
+    {
+        let random:UInt32 = arc4random_uniform(countBubbles)
+        let randomInt:Int = Int(random)
+        let bubble:MOptionPollutedGardenBubbleType = bubbleTypes[randomInt]
+        
+        return bubble
+    }
+    
+    private func randomColour() -> UIColor
+    {
+        let random:UInt32 = arc4random_uniform(countColours)
+        let randomInt:Int = Int(random)
+        let colour:UIColor = colours[randomInt]
+        
+        return colour
+    }
+    
+    private func randomPosition() -> CGPoint
+    {
+        let position:CGPoint = CGPoint(x:100, y:500)
+        
+        return position
+    }
+    
+    //MARK: public
+    
+    func randomBubble() -> MOptionPollutedGardenBubble
+    {
+        let bubbleType:MOptionPollutedGardenBubbleType = randomType()
+        let colour:UIColor = randomColour()
+        let position:CGPoint = randomPosition()
+        let bubble:MOptionPollutedGardenBubble = MOptionPollutedGardenBubble(
+            controller:controller,
+            bubbleType:bubbleType,
+            colour:colours,
+            position:position)
+        
+        return bubble
     }
 }
