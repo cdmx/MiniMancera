2c2a872c4c9ba6ae00da3f2832790fdb0a8fdf4f
vauxhall <zero.griffin@gmail.com>
2017-06-22 17:41:17 -0500

    base view

--- miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesBase.swift
+++ miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesBase.swift
@@ -3,40 +3,27 @@ import SpriteKit
 class VOptionWhistlesVsZombiesBase:SKSpriteNode
 {
     private weak var controller:COptionWhistlesVsZombies!
-    private let k
+    private weak var model:MOptionWhistlesVsZombiesBaseItem!
     
-    init(controller:COptionWhistlesVsZombies)
+    init(
+        controller:COptionWhistlesVsZombies,
+        model:MOptionWhistlesVsZombiesBaseItem)
     {
         self.controller = controller
+        self.model = model
         
-        super.init(texture:texture, color:UIColor.clear, size:size)
-        position = CGPoint(x:positionX, y:kInitialY)
-        zPosition = kZPosition
+        let base:MOptionWhistlesVsZombiesBase = controller.model.base
+        
+        super.init(
+            texture:base.texture,
+            color:UIColor.clear,
+            size:base.textureSize)
+        zPosition = MOptionWhistlesVsZombiesZPosition.Base
+        position = CGPoint(x:model.positionX, y:model.positionY)
     }
     
     required init?(coder:NSCoder)
     {
         return nil
-    }
-    
-    //MARK: private
-    
-    private func randomDuration() -> TimeInterval
-    {
-        let random:UInt32 = arc4random_uniform(kMaxAnimationduration)
-        let randomTime:TimeInterval = TimeInterval(random) / kAnimationDurationDivider
-        let totalTime:TimeInterval = randomTime + kMinAnimationDuration
-        
-        return totalTime
-    }
-    
-    //MARK: public
-    
-    func animateAppear()
-    {
-        let duration:TimeInterval = randomDuration()
-        let actionTranslate:SKAction = SKAction.move(to:endingPoint, duration:duration)
-        
-        run(actionTranslate)
     }
 }
