dd1459e6cc9b18bb7beee290f56d95e43176ac47
vauxhall <zero.griffin@gmail.com>
2017-06-22 11:40:56 -0500

    model whistles vs zombies

--- miniMancera/Model/Game/MGameProtocol.swift
+++ miniMancera/Model/Game/MGameProtocol.swift
@@ -2,10 +2,10 @@ import SpriteKit
 
 protocol MGameProtocol
 {
+    var soundBackground:String? { get }
     var size:CGSize { get }
     var score:Int { get }
     var gameActive:Bool { get }
-    var soundBackground:String? { get }
     
     init()
     func activateGame()
--- miniMancera/Model/Option/WhistlesVsZombies/Abstract/MOptionWhistlesVsZombies.swift
+++ miniMancera/Model/Option/WhistlesVsZombies/Abstract/MOptionWhistlesVsZombies.swift
@@ -1,9 +1,28 @@
-//
-//  MOptionWhistlesVsZombies.swift
-//  miniMancera
-//
-//  Created by zero on 6/22/17.
-//  Copyright © 2017 iturbide. All rights reserved.
-//
+import SpriteKit
 
-import Foundation
+class MOptionWhistlesVsZombies:MGameProtocol
+{
+    private(set) var soundBackground:String?
+    private(set) var size:CGSize
+    private(set) var score:Int
+    private(set) var gameActive:Bool
+    
+    required init()
+    {
+        size = CGSize.zero
+        score = 0
+        gameActive = false
+    }
+    
+    //MARK: game protocol
+    
+    func activateGame()
+    {
+        
+    }
+    
+    func sceneWithSize(controller:UIViewController, size:CGSize) -> SKScene?
+    {
+        return nil
+    }
+}
