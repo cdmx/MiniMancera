1ae56f013d993a15c72f1420525b5237734dd4fe
vauxhall <zero.griffin@gmail.com>
2017-06-22 12:16:20 -0500

    added child background

--- miniMancera/View/Option/WhistlesVsZombies/Scene/VOptionWhistlesVsZombiesScene.swift
+++ miniMancera/View/Option/WhistlesVsZombies/Scene/VOptionWhistlesVsZombiesScene.swift
@@ -2,5 +2,18 @@ import SpriteKit
 
 class VOptionWhistlesVsZombiesScene:ViewGame<MOptionWhistlesVsZombies, COptionWhistlesVsZombies>
 {
+    override init(controller:COptionWhistlesVsZombies)
+    {
+        super.init(controller:controller)
+        
+        let background:VOptionWhistlesVsZombiesBackground = VOptionWhistlesVsZombiesBackground(
+            controller:controller)
+        
+        addChild(background)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
