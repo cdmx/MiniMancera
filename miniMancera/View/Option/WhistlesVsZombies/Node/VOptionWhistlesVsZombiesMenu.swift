7d246e4b4ae24b2fb8e730300cf59fae462c5fdd
vauxhall <zero.griffin@gmail.com>
2017-06-22 19:05:50 -0500

    menu animations

--- miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesMenu.swift
+++ miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesMenu.swift
@@ -3,14 +3,19 @@ import SpriteKit
 class VOptionWhistlesVsZombiesMenu:SKSpriteNode
 {
     private weak var controller:COptionWhistlesVsZombies!
+    private let animationAppear:SKAction
+    private let animationDisappear:SKAction
     private let kAlpha:CGFloat = 0.8
     
     init(controller:COptionWhistlesVsZombies)
     {
         let backgroundColour:UIColor = UIColor(white:0, alpha:kAlpha)
         let size:CGSize = controller.model.size
+        animationAppear = VOptionWhistlesVsZombiesMenu.factoryAnimationAppear()
+        animationDisappear = VOptionWhistlesVsZombiesMenu.factoryAnimationDisappear()
         
         super.init(texture:nil, color:backgroundColour, size:size)
+        alpha = 0
     }
     
     required init?(coder:NSCoder)
