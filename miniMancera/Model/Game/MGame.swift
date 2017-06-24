1e0750308489099027682fd63db428581e5e066d
vauxhall <zero.griffin@gmail.com>
2017-06-24 18:08:44 -0500

    Refactor model game protocol

---
+++ miniMancera/Model/Game/MGame.swift
@@ -0,0 +1,23 @@
+import SpriteKit
+
+class MGame
+{
+    static let sceneSize:CGSize = UIScreen.main.bounds.size
+    
+    private(set) var soundBackground:String?
+    private(set) var score:Int
+    private(set) var gameActive:Bool
+    
+    required init()
+    {
+        score = 0
+        gameActive = false
+    }
+    
+    //MARK: public
+    
+    func activateGame()
+    {
+        gameActive = true
+    }
+}
