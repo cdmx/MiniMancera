85d7d60ed0a3bf6ac52ee3ac3ca21efab747550f
vauxhall <zero.griffin@gmail.com>
2017-06-26 18:03:37 -0500

    Action fadeOut

--- miniMancera/Model/Option/ReformaCrossing/Abstract/MOptionReformaCrossingActions.swift
+++ miniMancera/Model/Option/ReformaCrossing/Abstract/MOptionReformaCrossingActions.swift
@@ -3,10 +3,12 @@ import SpriteKit
 class MOptionReformaCrossingActions
 {
     let actionFadeIn:SKAction
+    let actionFadeOut:SKAction
     private let kFadeInDuration:TimeInterval = 0.5
     
     init()
     {
         actionFadeIn = SKAction.fadeIn(withDuration:kFadeInDuration)
+        actionFadeOut = SKAction.fadeOut(withDuration:kFadeInDuration)
     }
 }
