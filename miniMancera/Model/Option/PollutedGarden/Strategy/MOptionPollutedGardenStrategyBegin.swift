e5f03ee4a6040eadea391df7b28c7a54ebcb6a74
vauxhall <zero.griffin@gmail.com>
2017-06-29 13:34:30 -0500

    Strategy begin

---
+++ miniMancera/Model/Option/PollutedGarden/Strategy/MOptionPollutedGardenStrategyBegin.swift
@@ -0,0 +1,33 @@
+import SpriteKit
+
+class MOptionPollutedGardenStrategyBegin:MGameStrategyMain<MOptionPollutedGarden>
+{
+    private let kTimeout:TimeInterval = 1.5
+    
+    init(model:MOptionPollutedGarden)
+    {
+        let updateItems:[MGameUpdateProtocol] = [
+            ]
+        
+        super.init(model:model, updateItems:updateItems)
+    }
+    
+    override func update(
+        elapsedTime:TimeInterval,
+        scene:SKScene)
+    {
+        super.update(elapsedTime:elapsedTime, scene:scene)
+        
+        if elapsedTime > kTimeout
+        {
+            timeout(scene:scene)
+        }
+    }
+    
+    //MARK: private
+    
+    private func timeout(scene:SKScene)
+    {
+        model.activateGame()
+    }
+}
