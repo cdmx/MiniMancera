109f5ee55ee9b3220cbd9cf86efc20d886808527
vauxhall <zero.griffin@gmail.com>
2017-07-02 13:45:24 -0500

    View poison

---
+++ miniMancera/View/Option/PollutedGarden/Node/VOptionPollutedGardenPoison.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+class VOptionPollutedGardenPoison:ViewGameNode<MOptionPollutedGarden>
+{
+    private weak var model:MOptionPollutedGardenPlantCollect?
+    
+    init(
+        controller:ControllerGame<MOptionPollutedGarden>,
+        model:MOptionPollutedGardenPlantCollect)
+    {
+        self.model = model
+        let texture:MGameTexture = controller.model.textures.collect
+        
+        super.init(
+            controller:controller,
+            texture:texture)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    override func positionStart()
+    {
+        guard
+            
+            let model:MOptionPollutedGardenPlantCollect = self.model
+            
+            else
+        {
+            return
+        }
+        
+        position = model.position()
+    }
+}
