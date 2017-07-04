64b5a1f1f6a464dbcdb3a957b65693da32e05186
vauxhall <zero.griffin@gmail.com>
2017-07-04 03:21:07 -0500

    Strategies

--- miniMancera/Model/Option/WhistlesVsZombies/SonicBoom/MOptionWhistlesVsZombiesSonicBoomItem.swift
+++ miniMancera/Model/Option/WhistlesVsZombies/SonicBoom/MOptionWhistlesVsZombiesSonicBoomItem.swift
@@ -2,5 +2,42 @@ import Foundation
 
 class MOptionWhistlesVsZombiesSonicBoomItem:MGameUpdate<MOptionWhistlesVsZombies>
 {
+    private weak var base:MOptionWhistlesVsZombiesWhistleBase!
+    private weak var whistleType:MOptionWhistlesVsZombiesWhistleType!
+    private var strategy:MGameStrategy<MOptionWhistlesVsZombiesSonicBoomItem, MOptionWhistlesVsZombies>?
     
+    init(
+        base:MOptionWhistlesVsZombiesWhistleBase,
+        whistleType:MOptionWhistlesVsZombiesWhistleType)
+    {
+        self.base = base
+        self.whistleType = whistleType
+        super.init()
+        
+        strategy = MOptionWhistlesVsZombiesSonicBoomItemStrategyRelease(
+            model:self)
+    }
+    
+    override func update(
+        elapsedTime:TimeInterval,
+        scene:ViewGameScene<MOptionWhistlesVsZombies>)
+    {
+        strategy?.update(
+            elapsedTime:elapsedTime,
+            scene:scene)
+    }
+    
+    //MARK: public
+    
+    func moving()
+    {
+        strategy = MOptionWhistlesVsZombiesSonicBoomItemStrategyMoving(
+            model:self)
+    }
+    
+    func collision()
+    {
+        strategy = MOptionWhistlesVsZombiesSonicBoomItemStrategyCollision(
+            model:self)
+    }
 }
