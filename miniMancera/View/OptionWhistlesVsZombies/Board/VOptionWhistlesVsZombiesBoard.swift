f9287b97769bfa71555035def946ed3f0f261c73
vauxhall <zero.griffin@gmail.com>
2017-07-04 18:49:46 -0500

    Add bar view to board

--- miniMancera/View/OptionWhistlesVsZombies/Board/VOptionWhistlesVsZombiesBoard.swift
+++ miniMancera/View/OptionWhistlesVsZombies/Board/VOptionWhistlesVsZombiesBoard.swift
@@ -2,17 +2,42 @@ import UIKit
 
 class VOptionWhistlesVsZombiesBoard:View
 {
+    private let kBarHeight:CGFloat = 100
+    
     required init(controller:UIViewController)
     {
         super.init(controller:controller)
         backgroundColor = UIColor.clear
         
+        guard
+            
+            let controller:COptionWhistlesVsZombiesBoard = self.controller as? COptionWhistlesVsZombiesBoard
+        
+        else
+        {
+            return
+        }
+        
         let blur:VBlur = VBlur.dark()
         
+        let bar:VOptionWhistlesVsZombiesBoardBar = VOptionWhistlesVsZombiesBoardBar(
+            controller:controller)
+        
         addSubview(blur)
+        addSubview(bar)
         
         NSLayoutConstraint.equals(
             view:blur,
+            toView:self)
+        
+        NSLayoutConstraint.topToTop(
+            view:bar,
+            toView:self)
+        NSLayoutConstraint.height(
+            view:bar,
+            constant:kBarHeight)
+        NSLayoutConstraint.equalsHorizontal(
+            view:bar,
             toView:self)
     }
     
