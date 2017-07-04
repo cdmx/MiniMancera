5399c58a73b285fbda670f4dcfa7bb509c66592d
vauxhall <zero.griffin@gmail.com>
2017-07-04 18:09:19 -0500

    Board blur

--- miniMancera/View/OptionWhistlesVsZombies/Board/VOptionWhistlesVsZombiesBoard.swift
+++ miniMancera/View/OptionWhistlesVsZombies/Board/VOptionWhistlesVsZombiesBoard.swift
@@ -2,5 +2,22 @@ import UIKit
 
 class VOptionWhistlesVsZombiesBoard:View
 {
+    required init(controller:UIViewController)
+    {
+        super.init(controller:controller)
+        backgroundColor = UIColor.clear
+        
+        let blur:VBlur = VBlur.dark()
+        
+        addSubview(blur)
+        
+        NSLayoutConstraint.equals(
+            view:blur,
+            toView:self)
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
