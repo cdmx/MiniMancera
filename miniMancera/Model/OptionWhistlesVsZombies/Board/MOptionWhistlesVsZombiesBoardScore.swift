b148a3bee812c5321c7690796cbd8f3d87f8f4f7
vauxhall <zero.griffin@gmail.com>
2017-07-05 17:23:38 -0500

    Factory items speed and power

--- miniMancera/Model/OptionWhistlesVsZombies/Board/MOptionWhistlesVsZombiesBoardScore.swift
+++ miniMancera/Model/OptionWhistlesVsZombies/Board/MOptionWhistlesVsZombiesBoardScore.swift
@@ -1,11 +1,50 @@
-import Foundation
+import UIKit
 
 class MOptionWhistlesVsZombiesBoardScore
 {
     let items:[MOptionWhistlesVsZombiesBoardScoreItem]
+    private static let kSpeedDivider:TimeInterval = 10
+    private static let kPowerDivider:CGFloat = 10
     
     init(whistle:MOptionWhistlesVsZombiesWhistleProtocol)
     {
-        items = []
+        let itemSpeed:MOptionWhistlesVsZombiesBoardScoreItem = MOptionWhistlesVsZombiesBoardScore.factoryItemSpeed(
+            whistle:whistle)
+        let itemPower:MOptionWhistlesVsZombiesBoardScoreItem = MOptionWhistlesVsZombiesBoardScore.factoryItemPower(
+            whistle:whistle)
+        
+        items = [
+            itemSpeed,
+            itemPower]
+    }
+    
+    //MARK: private
+    
+    private class func factoryItemSpeed(whistle:MOptionWhistlesVsZombiesWhistleProtocol) -> MOptionWhistlesVsZombiesBoardScoreItem
+    {
+        let title:String = String.localizedWhistlesVsZombies(
+            key:"MOptionWhistlesVsZombiesBoardScore_titleSpeed")
+        let speed:TimeInterval = whistle.speed / kSpeedDivider
+        let speedAmount:Int = Int(round(speed))
+        
+        let item:MOptionWhistlesVsZombiesBoardScoreItem = MOptionWhistlesVsZombiesBoardScoreItem(
+            title:title,
+            amount:speedAmount)
+        
+        return item
+    }
+    
+    private class func factoryItemPower(whistle:MOptionWhistlesVsZombiesWhistleProtocol) -> MOptionWhistlesVsZombiesBoardScoreItem
+    {
+        let title:String = String.localizedWhistlesVsZombies(
+            key:"MOptionWhistlesVsZombiesBoardScore_titlePower")
+        let power:CGFloat = whistle.power / kPowerDivider
+        let powerAmount:Int = Int(round(power))
+        
+        let item:MOptionWhistlesVsZombiesBoardScoreItem = MOptionWhistlesVsZombiesBoardScoreItem(
+            title:title,
+            amount:powerAmount)
+        
+        return item
     }
 }
