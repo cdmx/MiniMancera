7d04b8f103c8a4013f05344edb0952f81c14496f
vauxhall <zero.griffin@gmail.com>
2017-07-06 15:17:19 -0500

    Repeat sonic boom animation

--- miniMancera/Model/Option/WhistlesVsZombies/Abstract/MOptionWhistlesVsZombiesActions.swift
+++ miniMancera/Model/Option/WhistlesVsZombies/Abstract/MOptionWhistlesVsZombiesActions.swift
@@ -7,8 +7,10 @@ class MOptionWhistlesVsZombiesActions
     let actionFadeOutIn:SKAction
     let transitionCrossFade:SKTransition
     private(set) var actionSonicReleaseAnimation:SKAction!
+    private(set) var actionSonicBoomAnimation:SKAction!
     private let kFadeDuration:TimeInterval = 0.3
     private let kSonicReleaseFrameDuration:TimeInterval = 0.1
+    private let kSonicBoomFrameDuration:TimeInterval = 0.1
     private let kSceneTransitionDuration:TimeInterval = 1
     
     init()
@@ -34,10 +36,23 @@ class MOptionWhistlesVsZombiesActions
             restore:false)
     }
     
+    private func factorySonicBoomAnimation(texture:MGameTextureAnimated)
+    {
+        let animation:SKAction = SKAction.animate(
+            with:texture.textures,
+            timePerFrame:kSonicBoomFrameDuration,
+            resize:false,
+            restore:false)
+        
+        actionSonicBoomAnimation = SKAction.repeatForever(
+            animation)
+    }
+    
     //MARK: public
     
     func factoryAnimations(textures:MOptionWhistlesVsZombiesTextures)
     {
         factorySonicReleaseAnimation(texture:textures.sonicReleaseAnimated)
+        factorySonicBoomAnimation(texture:textures.sonicBoomAnimated)
     }
 }
