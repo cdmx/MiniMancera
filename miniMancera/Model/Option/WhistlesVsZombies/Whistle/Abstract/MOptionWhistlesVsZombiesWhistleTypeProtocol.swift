2140350dd80d7dc7477eadd877eb6ce223630076
vauxhall <zero.griffin@gmail.com>
2017-07-05 20:33:40 -0500

    Crete whistle type protocol

---
+++ miniMancera/Model/Option/WhistlesVsZombies/Whistle/Abstract/MOptionWhistlesVsZombiesWhistleTypeProtocol.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+protocol MOptionWhistlesVsZombiesWhistleTypeProtocol:class
+{
+    var whistle:MOptionWhistlesVsZombiesWhistleProtocol { get }
+    var textureGame:MGameTexture { get }
+    var colour:UIColor { get }
+    var barrelLength:CGFloat { get }
+}
