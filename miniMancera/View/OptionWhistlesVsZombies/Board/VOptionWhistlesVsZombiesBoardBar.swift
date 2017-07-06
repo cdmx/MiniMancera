df95a8055fba1cecf8ab1eb8f983310609715252
vauxhall <zero.griffin@gmail.com>
2017-07-05 19:07:15 -0500

    Add coins amount to bar

--- miniMancera/View/OptionWhistlesVsZombies/Board/VOptionWhistlesVsZombiesBoardBar.swift
+++ miniMancera/View/OptionWhistlesVsZombies/Board/VOptionWhistlesVsZombiesBoardBar.swift
@@ -4,6 +4,8 @@ class VOptionWhistlesVsZombiesBoardBar:UIView
 {
     private weak var controller:COptionWhistlesVsZombiesBoard!
     private let kCancelWidth:CGFloat = 80
+    private let kCoinWidth:CGFloat = 50
+    private let kCoinLabelWidth:CGFloat = 150
     
     init(controller:COptionWhistlesVsZombiesBoard)
     {
@@ -36,8 +38,30 @@ class VOptionWhistlesVsZombiesBoardBar:UIView
             action:#selector(actionCancel(sender:)),
             for:UIControlEvents.touchUpInside)
         
+        let imageCoin:UIImageView = UIImageView()
+        imageCoin.isUserInteractionEnabled = false
+        imageCoin.translatesAutoresizingMaskIntoConstraints = false
+        imageCoin.clipsToBounds = true
+        imageCoin.contentMode = UIViewContentMode.center
+        imageCoin.image = #imageLiteral(resourceName: "assetWhistlesVsZombiesBoardPrice")
+        
+        let labelCoins:UILabel = UILabel()
+        labelCoins.isUserInteractionEnabled = false
+        labelCoins.translatesAutoresizingMaskIntoConstraints = false
+        labelCoins.backgroundColor = UIColor.clear
+        labelCoins.textAlignment = NSTextAlignment.right
+        labelCoins.font = UIFont.game(size:17)
+        labelCoins.textColor = UIColor(
+            red:0.972549019607843,
+            green:0.905882352941176,
+            blue:0.109803921568627,
+            alpha:1)
+        labelCoins.text = "12434"
+        
         addSubview(labelTitle)
+        addSubview(labelCoins)
         addSubview(buttonCancel)
+        addSubview(imageCoin)
         
         NSLayoutConstraint.equals(
             view:labelTitle,
@@ -46,12 +70,32 @@ class VOptionWhistlesVsZombiesBoardBar:UIView
         NSLayoutConstraint.equalsVertical(
             view:buttonCancel,
             toView:self)
-        NSLayoutConstraint.rightToRight(
+        NSLayoutConstraint.leftToLeft(
             view:buttonCancel,
             toView:self)
         NSLayoutConstraint.width(
             view:buttonCancel,
             constant:kCancelWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:imageCoin,
+            toView:self)
+        NSLayoutConstraint.rightToRight(
+            view:imageCoin,
+            toView:self)
+        NSLayoutConstraint.width(
+            view:imageCoin,
+            constant:kCoinWidth)
+        
+        NSLayoutConstraint.equalsVertical(
+            view:labelCoins,
+            toView:self)
+        NSLayoutConstraint.rightToLeft(
+            view:labelCoins,
+            toView:imageCoin)
+        NSLayoutConstraint.width(
+            view:labelCoins,
+            constant:kCoinLabelWidth)
     }
     
     required init?(coder:NSCoder)
