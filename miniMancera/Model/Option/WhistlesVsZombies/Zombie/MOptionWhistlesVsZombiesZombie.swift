a9d432ae868abd4157b889f3f4421044134a60ab
vauxhall <zero.griffin@gmail.com>
2017-07-07 01:44:35 -0500

    Model zombie

---
+++ miniMancera/Model/Option/WhistlesVsZombies/Zombie/MOptionWhistlesVsZombiesZombie.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+class MOptionWhistlesVsZombiesZombie:MGameUpdate<MOptionWhistlesVsZombies>
+{
+    
+}
--- miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesZombie.swift
+++ miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesZombie.swift
@@ -2,5 +2,13 @@ import Foundation
 
 class VOptionWhistlesVsZombiesZombie:ViewGameNode<MOptionWhistlesVsZombies>
 {
+    override init(controller:ControllerGame<MOptionWhistlesVsZombies>)
+    {
+        
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
 }
