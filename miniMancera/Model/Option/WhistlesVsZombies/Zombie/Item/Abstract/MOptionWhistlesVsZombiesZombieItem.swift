547ddceb307b8b3f279708b7a58b92ed48946524
vauxhall <zero.griffin@gmail.com>
2017-07-07 15:34:41 -0500

    Add strategy to zombie

--- miniMancera/Model/Option/WhistlesVsZombies/Zombie/Item/Abstract/MOptionWhistlesVsZombiesZombieItem.swift
+++ miniMancera/Model/Option/WhistlesVsZombies/Zombie/Item/Abstract/MOptionWhistlesVsZombiesZombieItem.swift
@@ -5,6 +5,7 @@ class MOptionWhistlesVsZombiesZombieItem:MGameUpdate<MOptionWhistlesVsZombies>
     weak var view:VOptionWhistlesVsZombiesZombie?
     private(set) weak var type:MOptionWhistlesVsZombiesZombieItemProtocol!
     private weak var lane:MOptionWhistlesVsZombiesGroundLane!
+    private var strategy:MGameStrategy<MOptionWhistlesVsZombiesZombieItem, MOptionWhistlesVsZombies>?
     private var positionX:CGFloat
     
     init(
@@ -17,18 +18,31 @@ class MOptionWhistlesVsZombiesZombieItem:MGameUpdate<MOptionWhistlesVsZombies>
         let sceneWidth:CGFloat = MGame.sceneSize.height
         let width_2:CGFloat = type.textureStand.width_2
         positionX = sceneWidth - width_2
+        
+        super.init()
+        
+        wait()
+    }
+    
+    override func update(
+        elapsedTime:TimeInterval,
+        scene:ViewGameScene<MOptionWhistlesVsZombies>)
+    {
+        strategy?.update(
+            elapsedTime:elapsedTime,
+            scene:scene)
     }
     
     //MARK: public
     
     func wait()
     {
-        
+        strategy = MOptionWhistlesVsZombiesZombieItemStrategyWait(model:self)
     }
     
     func walk()
     {
-        
+        strategy = MOptionWhistlesVsZombiesZombieItemStrategyWalk(model:self)
     }
     
     func position() -> CGPoint
