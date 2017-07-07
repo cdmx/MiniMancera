fb8f179943cf15d4d4aa34b17cd2483e5964e246
vauxhall <zero.griffin@gmail.com>
2017-07-07 16:21:31 -0500

    Add physics to zombies

--- miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesZombie.swift
+++ miniMancera/View/Option/WhistlesVsZombies/Node/VOptionWhistlesVsZombiesZombie.swift
@@ -13,6 +13,7 @@ class VOptionWhistlesVsZombiesZombie:ViewGameNode<MOptionWhistlesVsZombies>
         super.init(
             controller:controller,
             texture:model.type.textureStand)
+        startPhysics()
     }
     
     required init?(coder:NSCoder)
@@ -32,6 +33,35 @@ class VOptionWhistlesVsZombiesZombie:ViewGameNode<MOptionWhistlesVsZombies>
         }
         
         position = model.position()
+    }
+    
+    //MARK: private
+    
+    private func startPhysics()
+    {
+        guard
+            
+            let model:MOptionWhistlesVsZombiesZombieItem = self.model,
+            let modelTexture:MGameTexture = self.modelTexture
+            
+        else
+        {
+            return
+        }
+        
+        let size:CGSize = modelTexture.size
+        let physicsBody:SKPhysicsBody = SKPhysicsBody(rectangleOf:size)
+        physicsBody.isDynamic = true
+        physicsBody.friction = 0
+        physicsBody.allowsRotation = false
+        physicsBody.restitution = 1
+        physicsBody.angularVelocity = 0
+        //        physicsBody.density = model.mass
+        
+        physicsBody.categoryBitMask = MOptionWhistlesVsZombiesPhysicsStruct.Zombie
+        physicsBody.contactTestBitMask = MOptionWhistlesVsZombiesPhysicsStruct.None
+        physicsBody.collisionBitMask = MOptionWhistlesVsZombiesPhysicsStruct.None
+        self.physicsBody = physicsBody
     }
     
     //MARK: public
