da66abfefa48b504caf4c68afbf7d29b94bf7147
vauxhall <zero.griffin@gmail.com>
2017-07-10 09:28:26 -0500

    Create model cop

---
+++ miniMancera/Model/Option/WhistlesVsZombies/Zombie/Item/Type/MOptionWhistlesVsZombiesZombieItemCop.swift
@@ -0,0 +1,29 @@
+import SpriteKit
+
+class MOptionWhistlesVsZombiesZombieItemCop:MOptionWhistlesVsZombiesZombieItemProtocol
+{
+    private(set) weak var textureStand:MGameTexture!
+    private(set) weak var animatedWalking:SKAction!
+    private(set) var animatedDefeat:SKAction!
+    private(set) var speed:Int
+    private(set) var intelligence:Int
+    private(set) var life:Int
+    private(set) var coins:Int
+    private let kSpeed:Int = 7
+    private let kIntelligence:Int = 2
+    private let kLife:Int = 25
+    private let kCoins:Int = 1
+    
+    required init(
+        textures:MOptionWhistlesVsZombiesTextures,
+        actions:MOptionWhistlesVsZombiesActions)
+    {
+        textureStand = textures.zombieFootballStand
+        animatedWalking = actions.actionZombieFootballWakingAnimation
+        animatedDefeat = actions.actionZombieFootballDefeatedAnimation
+        speed = kSpeed
+        intelligence = kIntelligence
+        life = kLife
+        coins = kCoins
+    }
+}
