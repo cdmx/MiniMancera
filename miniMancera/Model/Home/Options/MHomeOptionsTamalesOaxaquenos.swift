2e05c2905ae6687f0c67abe8711e5792cfdd58e2
vauxhall <zero.griffin@gmail.com>
2017-07-12 13:46:40 -0500

    Create home model

---
+++ miniMancera/Model/Home/Options/MHomeOptionsTamalesOaxaquenos.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class MHomeOptionsTamalesOaxaquenos:MHomeOptions
+{
+    override var title:String?
+    {
+        get
+        {
+            return String.localizedTamalesOaxaquenos(
+                key:"MHomeOptionsTamalesOaxaquenos_title")
+        }
+    }
+    
+    override func gameController() -> UIViewController?
+    {
+        let controller:UIViewController? = gameControllerWith(type:COptionWhistlesVsZombies.self)
+        
+        return controller
+    }
+}
