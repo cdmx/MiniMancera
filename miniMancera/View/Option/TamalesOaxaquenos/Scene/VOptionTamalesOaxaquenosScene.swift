1b48ae43608835aee32cbc2d898b74f1e385c669
vauxhall <zero.griffin@gmail.com>
2017-07-14 18:58:49 -0500

    Factory nodes in scene

--- miniMancera/View/Option/TamalesOaxaquenos/Scene/VOptionTamalesOaxaquenosScene.swift
+++ miniMancera/View/Option/TamalesOaxaquenos/Scene/VOptionTamalesOaxaquenosScene.swift
@@ -2,5 +2,31 @@ import Foundation
 
 class VOptionTamalesOaxaquenosScene:ViewGameScene<MOptionTamalesOaxaquenos>
 {
+    required init(controller:ControllerGame<MOptionTamalesOaxaquenos>)
+    {
+        super.init(controller:controller)
+        factoryNodes()
+    }
     
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryNodes()
+    {
+        factoryArea()
+    }
+    
+    private func factoryArea()
+    {
+        let area:MOptionTamalesOaxaquenosArea = controller.model.area
+        
+        for item:MOptionTamalesOaxaquenosAreaItemProtocol in area.items
+        {
+            
+        }
+    }
 }
