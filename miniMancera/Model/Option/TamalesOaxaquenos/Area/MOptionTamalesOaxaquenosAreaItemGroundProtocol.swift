0dc5c218281b5d9c57e304926075f3dd7d515581
vauxhall <zero.griffin@gmail.com>
2017-07-14 17:25:57 -0500

    Create area item ground prot

---
+++ miniMancera/Model/Option/TamalesOaxaquenos/Area/MOptionTamalesOaxaquenosAreaItemGroundProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  MOptionTamalesOaxaquenosAreaItemGroundProtocol.swift
+//  miniMancera
+//
+//  Created by zero on 7/14/17.
+//  Copyright © 2017 iturbide. All rights reserved.
+//
+
+import Foundation
