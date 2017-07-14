e074e40aab016deb80a72163b51287420e48d391
vauxhall <zero.griffin@gmail.com>
2017-07-14 17:03:30 -0500

    Create model textures

--- miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenos.swift
+++ miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenos.swift
@@ -2,6 +2,15 @@ import SpriteKit
 
 class MOptionTamalesOaxaquenos:MGame
 {
+    let area:MOptionTamalesOaxaquenosArea
+    
+    required init()
+    {
+        area = MOptionTamalesOaxaquenosArea()
+        
+        super.init()
+    }
+    
     override var startSceneType:SKScene.Type?
     {
         get
