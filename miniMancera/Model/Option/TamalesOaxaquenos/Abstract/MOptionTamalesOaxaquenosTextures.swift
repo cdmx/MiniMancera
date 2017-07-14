035fca1d537b1341b3c3843766be9a92a7cc7108
vauxhall <zero.griffin@gmail.com>
2017-07-14 17:06:35 -0500

    Create model zPositions

--- miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenosTextures.swift
+++ miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenosTextures.swift
@@ -2,6 +2,12 @@ import UIKit
 
 class MOptionTamalesOaxaquenosTextures
 {
+    let ground0:MGameTexture
+    let ground1:MGameTexture
+    let ground2:MGameTexture
+    let ground3:MGameTexture
+    let grass:MGameTexture
+    
     init()
     {
         
---
+++ miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenosZPosition.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+enum MOptionTamalesOaxaquenosZPosition:CGFloat
+{
+    case Ground
+}
