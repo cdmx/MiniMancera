f44c05e33cc03a2cdc301d46208cf6eca0a2b275
vauxhall <zero.griffin@gmail.com>
2017-07-14 16:23:31 -0500

    Create sprite atlas

--- miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenos.swift
+++ miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenos.swift
@@ -1,6 +1,12 @@
-import Foundation
+import SpriteKit
 
 class MOptionTamalesOaxaquenos:MGame
 {
-    
+    override var startSceneType:SKScene.Type?
+    {
+        get
+        {
+            return VOptionTamalesOaxaquenosScene.self
+        }
+    }
 }
