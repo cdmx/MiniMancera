466dc49c8eb4eb76229dfd4f929489d7dfe548b4
vauxhall <zero.griffin@gmail.com>
2017-07-15 05:48:55 -0500

    Create node perspective

---
+++ miniMancera/View/Option/TamalesOaxaquenos/Node/VOptionTamalesOaxaquenosPerspective.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+class VOptionTamalesOaxaquenosPerspective:ViewGameNode<MOptionTamalesOaxaquenos>
+{
+    override init(controller:ControllerGame<MOptionTamalesOaxaquenos>)
+    {
+        
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
