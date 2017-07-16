595e222be6faf6b8f041a4c068ba512931c2d2e0
vauxhall <zero.griffin@gmail.com>
2017-07-15 19:26:29 -0500

    Add player to model

--- miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenos.swift
+++ miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenos.swift
@@ -5,12 +5,14 @@ class MOptionTamalesOaxaquenos:MGame
     let textures:MOptionTamalesOaxaquenosTextures
     let action:MOptionTamalesOaxaquenosActions
     let area:MOptionTamalesOaxaquenosArea
+    let player:MOptionTamalesOaxaquenosPlayer
     
     required init()
     {
         textures = MOptionTamalesOaxaquenosTextures()
         action = MOptionTamalesOaxaquenosActions(textures:textures)
         area = MOptionTamalesOaxaquenosArea(textures:textures)
+        player = MOptionTamalesOaxaquenosPlayer()
         
         super.init()
     }
