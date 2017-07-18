6bc149c0d11b7967281370974cc8561499e1e621
vauxhall <zero.griffin@gmail.com>
2017-07-18 11:39:49 -0500

    Create physics struct

---
+++ miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenosPhysicsStruct.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct MOptionTamalesOaxaquenosPhysicsStruct
+{
+    static let None:UInt32 =            0b0
+    static let Player:UInt32 =          0b1
+    static let Floor:UInt32 =           0b10
+}
--- miniMancera/Model/Option/WhistlesVsZombies/Abstract/MOptionWhistlesVsZombiesPhysicsStruct.swift
+++ miniMancera/Model/Option/WhistlesVsZombies/Abstract/MOptionWhistlesVsZombiesPhysicsStruct.swift
@@ -2,7 +2,7 @@ import Foundation
 
 struct MOptionWhistlesVsZombiesPhysicsStruct
 {
-    static let None:UInt32 =            0
+    static let None:UInt32 =            0b0
     static let SonicBoom:UInt32 =       0b1
     static let Zombie:UInt32 =          0b10
     static let Whistle:UInt32 =         0b100
