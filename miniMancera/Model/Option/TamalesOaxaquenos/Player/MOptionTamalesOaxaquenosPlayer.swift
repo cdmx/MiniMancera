cea103d54669454dd7e4077142ddf1a833ff37bf
vauxhall <zero.griffin@gmail.com>
2017-07-18 11:24:11 -0500

    Update strategy on player

--- miniMancera/Model/Option/TamalesOaxaquenos/Player/MOptionTamalesOaxaquenosPlayer.swift
+++ miniMancera/Model/Option/TamalesOaxaquenos/Player/MOptionTamalesOaxaquenosPlayer.swift
@@ -3,4 +3,33 @@ import Foundation
 class MOptionTamalesOaxaquenosPlayer:MGameUpdate<MOptionTamalesOaxaquenos>
 {
     weak var view:VOptionTamalesOaxaquenosPlayer?
+    private var strategy:MGameStrategy<MOptionTamalesOaxaquenosPlayer, MOptionTamalesOaxaquenos>?
+    
+    override init()
+    {
+        super.init()
+        
+        stand()
+    }
+    
+    override func update(
+        elapsedTime:TimeInterval,
+        scene:ViewGameScene<MOptionTamalesOaxaquenos>)
+    {
+        strategy?.update(
+            elapsedTime:elapsedTime,
+            scene:scene)
+    }
+    
+    //MARK: public
+    
+    func stand()
+    {
+        strategy = MOptionTamalesOaxaquenosPlayerStrategyStand(model:self)
+    }
+    
+    func walk()
+    {
+        strategy = MOptionTamalesOaxaquenosPlayerStrategyWalking(model:self)
+    }
 }
