1f560549da5e16bbfb4b4a5885f8d4b29a11894a
vauxhall <zero.griffin@gmail.com>
2017-07-18 18:13:56 -0500

    Update controls

--- miniMancera/Model/Option/TamalesOaxaquenos/Strategy/MOptionTamalesOaxaquenosStrategyBegin.swift
+++ miniMancera/Model/Option/TamalesOaxaquenos/Strategy/MOptionTamalesOaxaquenosStrategyBegin.swift
@@ -8,6 +8,7 @@ class MOptionTamalesOaxaquenosStrategyBegin:MGameStrategyMain<MOptionTamalesOaxa
     init(model:MOptionTamalesOaxaquenos)
     {
         let updateItems:[MGameUpdate<MOptionTamalesOaxaquenos>] = [
+            model.controls,
             model.camera,
             model.player]
         
--- miniMancera/View/Option/TamalesOaxaquenos/Node/VOptionTamalesOaxaquenosControls.swift
+++ miniMancera/View/Option/TamalesOaxaquenos/Node/VOptionTamalesOaxaquenosControls.swift
@@ -14,7 +14,6 @@ class VOptionTamalesOaxaquenosControls:ViewGameNode<MOptionTamalesOaxaquenos>
             size:size,
             zPosition:MOptionTamalesOaxaquenosZPosition.Controls.rawValue)
         isUserInteractionEnabled = true
-        color = UIColor(red:1, green:0, blue:0, alpha:0.5)
     }
     
     required init?(coder:NSCoder)
