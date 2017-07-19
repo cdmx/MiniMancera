6a58e5918e9cab8ea3be0914277b9594fd0b9019
vauxhall <zero.griffin@gmail.com>
2017-07-19 15:22:14 -0500

    Position menu in camera

--- miniMancera/View/Option/TamalesOaxaquenos/Scene/VOptionTamalesOaxaquenosScene.swift
+++ miniMancera/View/Option/TamalesOaxaquenos/Scene/VOptionTamalesOaxaquenosScene.swift
@@ -2,6 +2,8 @@ import SpriteKit
 
 class VOptionTamalesOaxaquenosScene:ViewGameScene<MOptionTamalesOaxaquenos>
 {
+    private let kMenuPositionY:CGFloat = 45
+    
     required init(controller:ControllerGame<MOptionTamalesOaxaquenos>)
     {
         super.init(controller:controller)
@@ -38,14 +40,15 @@ class VOptionTamalesOaxaquenosScene:ViewGameScene<MOptionTamalesOaxaquenos>
             controller:controller,
             texture:model.textures.menu)
         model.viewMenu = menu
+        positionMenu(menu:menu)
 
         camera.addChild(background)
         camera.addChild(controls)
+        camera.addChild(menu)
         
         factoryArea()
         addChild(camera)
         addChild(player)
-        addChild(menu)
     }
     
     private func factoryArea()
@@ -86,5 +89,27 @@ class VOptionTamalesOaxaquenosScene:ViewGameScene<MOptionTamalesOaxaquenos>
             
             addChild(node)
         }
+    }
+    
+    private func positionMenu(menu:ViewGameNodeMenu<MOptionTamalesOaxaquenos>)
+    {
+        guard
+            
+            let modelTexture:MGameTexture = menu.modelTexture
+            
+        else
+        {
+            return
+        }
+        
+        let sceneSize:CGSize = MGame.sceneSize
+        let sceneWidth:CGFloat = sceneSize.width
+        let sceneHeight:CGFloat = sceneSize.height
+        let sceneWidth_2:CGFloat = sceneWidth / 2.0
+        let sceneHeight_2:CGFloat = sceneHeight / 2.0
+        let width_2:CGFloat = modelTexture.width_2
+        let positionX:CGFloat = width_2 - sceneWidth_2
+        let positionY:CGFloat = sceneHeight_2 - kMenuPositionY
+        menu.position = CGPoint(x:positionX, y:positionY)
     }
 }
