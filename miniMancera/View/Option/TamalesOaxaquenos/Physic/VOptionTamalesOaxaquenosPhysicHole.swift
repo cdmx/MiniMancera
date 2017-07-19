5a6c0c09461c1da9d4e996e2f7a6f3e668d9828d
vauxhall <zero.griffin@gmail.com>
2017-07-19 15:54:07 -0500

    Create physic hole

---
+++ miniMancera/View/Option/TamalesOaxaquenos/Physic/VOptionTamalesOaxaquenosPhysicHole.swift
@@ -0,0 +1,48 @@
+import SpriteKit
+
+class VOptionTamalesOaxaquenosPhysicHole:ViewGameNode<MOptionTamalesOaxaquenos>
+{
+    private let positionY:CGFloat
+    private let kWidth:CGFloat = 20
+    
+    override init(controller:ControllerGame<MOptionWhistlesVsZombies>)
+    {
+        let size:CGSize = CGSize(
+            width:kWidth,
+            height:kHeight)
+        width_2 = kWidth / 2.0
+        height_2 = kHeight / 2.0
+        
+        super.init(
+            controller:controller,
+            size:size,
+            zPosition:MOptionWhistlesVsZombiesZPosition.Physics.rawValue)
+        startPhysics()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+    
+    //MARK: private
+    
+    override func positionStart()
+    {
+        position = CGPoint(x:width_2, y:height_2)
+    }
+    
+    private func startPhysics()
+    {
+        let edgeFrame:CGRect = CGRect(
+            x:-width_2,
+            y:-height_2,
+            width:kWidth,
+            height:kHeight)
+        let physicsBody:SKPhysicsBody = SKPhysicsBody(edgeLoopFrom:edgeFrame)
+        physicsBody.categoryBitMask = MOptionWhistlesVsZombiesPhysicsStruct.Home
+        physicsBody.contactTestBitMask = MOptionWhistlesVsZombiesPhysicsStruct.Zombie
+        physicsBody.collisionBitMask = MOptionWhistlesVsZombiesPhysicsStruct.None
+        self.physicsBody = physicsBody
+    }
+}
