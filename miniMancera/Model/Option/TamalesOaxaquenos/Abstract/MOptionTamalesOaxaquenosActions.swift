6bd98f9a75b87439cf07a6899cba95e0256a4546
vauxhall <zero.griffin@gmail.com>
2017-07-19 15:02:51 -0500

    Create actions fade

--- miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenosActions.swift
+++ miniMancera/Model/Option/TamalesOaxaquenos/Abstract/MOptionTamalesOaxaquenosActions.swift
@@ -2,10 +2,26 @@ import SpriteKit
 
 class MOptionTamalesOaxaquenosActions
 {
+    let actionFadeIn:SKAction
+    let actionFadeOut:SKAction
+    let actionFadeOutIn:SKAction
     let actionPlayerWalkingAnimation:SKAction
+    let transitionCrossFade:SKTransition
+    private let kFadeDuration:TimeInterval = 0.3
+    private let kSceneTransitionDuration:TimeInterval = 1
     
     init(textures:MOptionTamalesOaxaquenosTextures)
     {
+        actionFadeIn = SKAction.fadeIn(withDuration:kFadeDuration)
+        actionFadeOut = SKAction.fadeOut(withDuration:kFadeDuration)
+        
+        let sequenceOutIn:[SKAction] = [
+            actionFadeOut,
+            actionFadeIn]
+        actionFadeOutIn = SKAction.sequence(sequenceOutIn)
+        
+        transitionCrossFade = SKTransition.crossFade(withDuration:kSceneTransitionDuration)
+        
         actionPlayerWalkingAnimation = MOptionTamalesOaxaquenosActions.factoryPlayerWalkingAnimation(
             texture:textures.playerWalkingAnimated)
     }
